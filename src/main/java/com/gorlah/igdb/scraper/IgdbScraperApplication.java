52050ec43c9b96e9ab260bb332c71d4ff62b5219
Eric <eah0592@gmail.com>
2020-12-09 09:12:59 -0600

    Initial commit

---
+++ src/main/java/com/gorlah/igdb/scraper/IgdbScraperApplication.java
@@ -0,0 +1,12 @@
+package com.gorlah.igdb.scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class IgdbScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgdbScraperApplication.class, args);
+    }
+}
---
+++ src/test/java/com/gorlah/igdb/scraper/IgdbScraperApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gorlah.igdb.scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgdbScraperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
